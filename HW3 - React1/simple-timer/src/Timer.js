ebb57a8bbcfa953320d4d3ea33d0fb22a10fbc6f
MJSahebnasi <mjsahebnasi@gmail.com>
2022-04-21 20:01:29 +0430

    timer works

--- HW3 - React1/simple-timer/src/Timer.js
+++ HW3 - React1/simple-timer/src/Timer.js
@@ -2,10 +2,38 @@ import React, {Component} from 'react';
 import './Timer.css';
 
 export class Timer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            time: 0
+        };
+    }
+
+    increaseTime(){
+        if (this.state.time === 59){
+            this.setState({
+                time: 0
+            });
+        }
+        else{
+            this.setState({
+                time: this.state.time+1
+            });
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            time: 0
+        });
+        setInterval(() => {
+            this.increaseTime();
+          }, 1000);
+    }
+
     render() {
         return <div className="container">
-            <div className="timer">
-            </div>
+            <div className="timer">{this.state.time}</div>
             <button>Reset Timer</button>
         </div>;
     }
