1579b0b56253a79808682d45ba402f57dfc6294d
MJSahebnasi <mjsahebnasi@gmail.com>
2022-04-22 10:28:13 +0430

    still foolin around

--- HW3 - React1/input-validation/src/input.js
+++ HW3 - React1/input-validation/src/input.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { validate } from './validators';
 
+// const inputValue = {name: null, username: null, email:null, age:null};
+// const inputValidators = {name: null, username: null, email:null, age:null};
 var inputValue;
 var inputValidators;
 
@@ -12,15 +14,21 @@ const INPUT_STATES = {
 
 const handleChange = (e) => {
   const {name, value} = e.target;
-  // var res = validate(value, v);
+  // console.log(e.target);
+  console.log(value);
+  console.log(inputValidators);
+
+  // var res = validate(value, Object.values(inputValidators));
+  var res = validate(value, inputValidators);
   // var res = Object.values(inputValidators).forEach(v => {
   //   validate(value, v)
   // });
-  // console.log(res)
+  // console.log(res);
 }
 
 const Input = props => {
   inputValidators = props.validators;
+  // console.log(props);
   // examination:
   // console.log(typeof validators);
   // console.log(Object.keys(validators));
@@ -29,7 +37,7 @@ const Input = props => {
   return (
     <div className='form-input' data-testid="form-input">
       <label>{props.label}</label>
-      <input value={inputValue} onChange={handleChange}/>
+      <input onChange={handleChange}/>
       <p></p>
     </div>
   )
