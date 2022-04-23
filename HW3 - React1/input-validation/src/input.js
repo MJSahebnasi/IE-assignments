b8383308669bf0f05e20dab9c8d5a732ce0f0f20
MJSahebnasi <mjsahebnasi@gmail.com>
2022-04-23 12:08:25 +0430

    almost DONE - focus/blur handle remaining

--- HW3 - React1/input-validation/src/input.js
+++ HW3 - React1/input-validation/src/input.js
@@ -4,36 +4,64 @@ import { useState } from 'react';
 
 const INPUT_STATES = {
   UNTOUCHED: 'UNTOUCHED',
-  VALID: 'VALID',
-  INVALID: 'INVALID'
+  TOUCHED: 'TOUCHED',
+  NORMAL: 'NORMAL'
 };
 
 const Input = props => {
   const [errorMessage, setErrorMessage] = useState("");
-  // const [n, setN] = useState(0);
+  let [state, setState] = useState(INPUT_STATES.UNTOUCHED);
+  // let [valid, setValid] = useState(null);
 
   // examination:
   // console.log(typeof inputValidators);
   // console.log(Object.keys(inputValidators));
   // console.log(Object.values(inputValidators));
 
+  const handleFocus = () => {
+    // console.log('in hndFcs');
+
+    // if (state === INPUT_STATES.UNTOUCHED){
+    //   setState(INPUT_STATES.TOUCHED);
+    //   console.log('unt -> touched');
+    // }
+  }
+  const handleBlur = () => {
+    // console.log('in hndBlr');
+
+    // if (state === INPUT_STATES.TOUCHED){
+    //   setState(INPUT_STATES.NORMAL);
+    //   console.log('touched -> normal');
+
+    //   if (valid){
+    //     setErrorMessage("");
+    //     const element = document.getElementById(props.id);
+    //     element.classList.remove("form-input--invalid");
+    //   }
+    //   if (!valid){
+    //     setErrorMessage(props.errorText);
+    //     const element = document.getElementById(props.id);
+    //     element.classList.add("form-input--invalid");
+    //   }
+    // }
+  }
   const handleChange = (e) => {
     const {name, value} = e.target;
-    let valid = validate(value, props.validators);
+    let validationRes = validate(value, props.validators);
+    // setValid(validationRes);
 
-    console.log(valid);
-    if (valid){
+    // if (valid && state === INPUT_STATES.NORMAL){
+    if (validationRes){
       setErrorMessage("");
       const element = document.getElementById(props.id);
       element.classList.remove("form-input--invalid");
     }
 
-    if (!valid){
+    // if (!valid && state === INPUT_STATES.NORMAL){
+    if (!validationRes){
       setErrorMessage(props.errorText);
       
-      // const myForm = document.getElementById(props.id).getElementsByClassName('form-input');
       const element = document.getElementById(props.id);
-      // console.log(element);
       element.classList.add("form-input--invalid");
     }
   };
@@ -41,8 +69,9 @@ const Input = props => {
   return (
     <div id={props.id} className='form-input' data-testid="form-input">
       <label>{props.label}</label>
-      <input onChange={handleChange}/>
+      <input onChange={handleChange} onFocus={handleFocus} onBlur={handleBlur}/>
       <p>{errorMessage}</p>
+      <p>{state}</p>
     </div>
   )
 };
