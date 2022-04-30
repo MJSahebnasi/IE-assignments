7fdc18b2f3b3ba097b85772889bf636df4bc6975
MJSahebnasi <mjsahebnasi@gmail.com>
2022-04-30 15:47:22 +0430

    BriefCard props done + buggy json load

--- HW4 - React2/shop/src/App.js
+++ HW4 - React2/shop/src/App.js
@@ -3,14 +3,22 @@ import './style/app.css';
 import './style/mainPage.css';
 import Navbar from './Navbar';
 import BriefCard from './BriefCard';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  let [productsData, setProductsData] = useState(JSON.parse('data/data.json'));
+
+  useEffect(() => {
+    setProductsData(JSON.parse('data/data.json'));
+  }, []);
+
   return (
     <div className='AppContainer'>
       <div className="App">
         <Navbar />
         <div className='briefCard_container'>
-          <BriefCard />
+          <BriefCard title={productsData.title}/>
           <BriefCard />
           <BriefCard />
           <BriefCard />
--- HW4 - React2/shop/src/BriefCard.js
+++ HW4 - React2/shop/src/BriefCard.js
@@ -7,16 +7,16 @@ class BriefCard extends React.Component {
         return (
             <div className='BriefCard'>
                 <div className='img_div'>
-                    
+                    <img src={this.props.img_url} alt={"cannot load"}/>
                 </div>
                 <div className='title_div'>
-
+                    {this.props.title}
                 </div>
                 <div className='price_div'>
-
+                    {this.props.price}
                 </div>
                 <div className='size_div'>
-
+                    {this.props.size}
                 </div>
                 <div className='button_div'>
                     <button type="button" class="add_to_cart_button">Add to Cart</button>
