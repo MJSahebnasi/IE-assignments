fe6f5ccf29c03e352d5539fe381dd4e6c6b56d19
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-03 14:12:51 +0430

    tiny change

--- HW4 - React2/shop/src/App.js
+++ HW4 - React2/shop/src/App.js
@@ -7,20 +7,20 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [data, setData] = useState();
+  const [cardsData, setData] = useState();
     useEffect(() => {
-        setData(require('./data/data.json'));
+        setData(require('./data/data.json').data);
     }, []);
 
-  if (!data)  
+  if (!cardsData)  
     return <p>loading ...</p>
   else
     // console.log(data);
     // console.log(data.data.length);
 
     var cards = [];
-    for (var i = 0; i < data.data.length; i++) {
-      cards.push(<BriefCard key={i} title={data.data[i].title} img_url={data.data[i].img} price={data.data[i].price} size={data.data[i].size}/>);
+    for (var i = 0; i < cardsData.length; i++) {
+      cards.push(<BriefCard key={i} title={cardsData[i].title} img_url={cardsData[i].img} price={cardsData[i].price} size={cardsData[i].size}/>);
     }
 
     return (
