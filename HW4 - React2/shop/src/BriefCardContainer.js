d2bdb90d5ae1bfd3e29881978fa25199a24eb340
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-06 22:27:53 +0430

    tiny change

--- HW4 - React2/shop/src/BriefCardContainer.js
+++ HW4 - React2/shop/src/BriefCardContainer.js
@@ -8,8 +8,6 @@ class BriefCardContainer extends React.Component {
         const cardComponents = [];
         const cardsData = this.props.cardsData;
 
-        // TODO: keep cardData, remove the rest props:
-
         for (var i = 0; i < cardsData.length; i++) {
             cardComponents.push(<BriefCard cardData={cardsData[i]} key={i} />);
         }
