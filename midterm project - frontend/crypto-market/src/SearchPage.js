0375a0118da3e9951c4c5120146bad397ac719ba
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-26 18:42:32 +0430

    search page halfway

--- midterm project - frontend/crypto-market/src/SearchPage.js
+++ midterm project - frontend/crypto-market/src/SearchPage.js
@@ -16,14 +16,19 @@ function SearchPage(props) {
             </div>
 
             <div id='searchPage_body'>
-                <div><h1> Cryptocurrency Prices by Market Cap </h1></div>
-                <div>
+                <div id='searchPage_body_inner_title'><h1> Cryptocurrency Prices by Market Cap </h1></div>
+                <div id='search_bar_div'>
                     <form>
-                        <input type={'text'}></input>
+                        <input type={'text'} id='search_bar' placeholder='search for a cryptocurrency by name or symbol ...'></input>
                     </form>
                 </div>
-                <div>
-                    
+                <div id='search_results'>
+                    <div id='search_results_header' className='search_results_row'>
+                        <span id='searchpage_coin_span'>Coin</span>
+                        <span>Price</span>
+                        <span>24h Change</span>
+                        <span>Market Cap</span>
+                    </div>
                 </div>
             </div>
 
