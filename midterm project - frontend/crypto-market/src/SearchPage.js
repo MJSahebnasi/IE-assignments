59896cf9e77c9b7e7e665b1f8e7fdd7109a3f220
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-26 13:03:23 +0430

    search page structure

--- midterm project - frontend/crypto-market/src/SearchPage.js
+++ midterm project - frontend/crypto-market/src/SearchPage.js
@@ -8,7 +8,24 @@ function SearchPage(props) {
 
             <MutualHeader />
 
-            
+            <div id='searchPage_title'>
+                <div>
+                    <h1>Search Coin</h1>
+                    get information from here
+                </div>
+            </div>
+
+            <div id='searchPage_body'>
+                <div><h1> Cryptocurrency Prices by Market Cap </h1></div>
+                <div>
+                    <form>
+                        <input type={'text'}></input>
+                    </form>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
 
         </div>
     );
