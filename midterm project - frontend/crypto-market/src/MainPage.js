8ee4966030657f45b896af8153288208edd18b3e
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-26 11:12:04 +0430

    main page structure - more details

--- midterm project - frontend/crypto-market/src/MainPage.js
+++ midterm project - frontend/crypto-market/src/MainPage.js
@@ -12,10 +12,25 @@ function MainPage(props) {
 
             <div id='MainPage_body'>
                 <div id='MainPage_left' className='MainPage_body_part'>
-                    <h1>Search & Buy Crypto</h1>
+                    <div id='mainpage_title'>
+                        <h1>Search & </h1> 
+                        <h1>Buy <span className='yellow_text'>Crypto</span> </h1>
+                    </div>
+                    <div>
+                        Shahid Beheshti University 
+                        <br></br>
+                        IE Final Project
+                    </div>
+                    <div>
+                        <button type='button' id='mainpage_search_button'>SEARCH MORE</button>
+                    </div>
                 </div>
                 <div id='MainPage_right' className='MainPage_body_part'>
-                    image
+                    <div id='last_searches'>
+                        <div> s1 </div>
+                        <div> s2 </div>
+                        <div> s3 </div>
+                    </div>
                 </div>
             </div>
 
