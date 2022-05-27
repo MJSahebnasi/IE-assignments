da658e7d68b00810047203fa4cd2b0436befbcc7
MJSahebnasi <mjsahebnasi@gmail.com>
2022-05-27 11:05:17 +0430

    mutual header almost done

--- midterm project - frontend/crypto-market/src/MutualHeader.js
+++ midterm project - frontend/crypto-market/src/MutualHeader.js
@@ -4,9 +4,12 @@ function MutualHeader(props) {
 
     return (
         <div className='mutual_header'>
-            <span id='text_span'>IE Final Project</span>
+            <span id='text_span'><b>IE Final Project</b></span>
             <span id='button_span'>
-                <button type='button'>change theme</button>
+                {/* this button's style is in MainPage.css (I've put the same button there too)
+                I knew it's better to make it a component to avoid duplicate code,
+                but I didn't have enough time for that */}
+                <button type='button' id='change_theme_button'>Change Theme</button>
             </span>
         </div>
     );
