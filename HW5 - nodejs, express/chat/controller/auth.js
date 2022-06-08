f962faa7f429040111fe1e131149565862a4e93f
MJSahebnasi <mjsahebnasi@gmail.com>
2022-06-08 22:32:16 +0430

    .env debugged

--- HW5 - nodejs, express/chat/controller/auth.js
+++ HW5 - nodejs, express/chat/controller/auth.js
@@ -5,13 +5,17 @@ const users = data.users;
 const entities = require('../entities')
 const User = entities.User;
 
+require("dotenv")
+  .config();
+
 exports.signup = function (name, email, password) {
     let userEmails = users.map(user => user.email);
     if (userEmails.includes(email))
         return undefined;
-    // let token = jwt.sign({name: name}, process.env.ACCESS_TOKEN_SECRET);
-    let token = jwt.sign({name: name}, 'secret_string');
-    let newUser = new User(users.length, name, email, password, token);
+
+    let id = users.length;
+    let token = jwt.sign({id: id}, process.env.ACCESS_TOKEN_SECRET);
+    let newUser = new User(id, name, email, password, token);
     users.push(newUser);
     return {token: token, message: 'successful'};
 };
