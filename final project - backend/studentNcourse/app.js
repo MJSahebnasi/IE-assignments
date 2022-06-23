01445803f2bce673f4233ca33db16cdcf5d3addd
MJSahebnasi <mjsahebnasi@gmail.com>
2022-06-23 19:09:07 +0430

    connecting to db

--- final project - backend/studentNcourse/app.js
+++ final project - backend/studentNcourse/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(express.json())
@@ -9,4 +11,7 @@ const coursesRouter = require('./routes/courses')
 app.use('/students', studentsRouter)
 app.use(coursesRouter)
 
+mongoose.connect('mongodb://localhost:27017/IE_final_proj')
+    .then('connected to DB.')
+    .catch(error => handleError(error));
 app.listen(3000, () => console.log('listening on port 3000 ...'))
