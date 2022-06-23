2966275548a64acc84a6437c00a65b3614a23255
MJSahebnasi <mjsahebnasi@gmail.com>
2022-06-23 23:09:20 +0430

    get stud info + update last_apdated for adding crs

--- final project - backend/studentNcourse/routes/courses.js
+++ final project - backend/studentNcourse/routes/courses.js
@@ -38,19 +38,29 @@ router.post("/:student_id/course", async(req, res) => {
     await Student.findOneAndUpdate({ student_id: student_id },
         { average: calc_avr(grades) }, { returnOriginal: false });
 
+    // updating last_updated
+    await Student.findOneAndUpdate({ student_id: student_id },
+        { last_updated: Date.now() }, { returnOriginal: false });
+
     res.send({name:req.body.name, id:req.body.id, grade:req.body.grade, code: 200, 
         message:"course added succesfully!"});
 })
 
-router.get("/:student_id", (req, res) => {
-    // get stud's info
-    res.status(200).send('one studs info');
-
-    // let result = login(req.body.email, req.body.password);
-    // if (result)
-    //     res.status(200).json(result);
-    // else
-    //     res.status(400).json(bad_req);
+router.get("/:student_id", async(req, res) => {
+    let student_id = req.params.student_id;
+    let students_with_id = await Student.find({ student_id: student_id });
+    if (students_with_id.length === 0) {
+        res.status(400).send(`there's no student with this ID (${student_id}) :(`);
+        return
+    }
+    res.status(200).json(students_with_id.map(
+        s => {
+            return {
+                studentid: s.student_id, average: s.average,
+                courses: s.courses, last_updated: s.last_updated
+            }
+        }
+    )[0]);
 })
 
 router.put("/:student_id/:course_id", (req, res) => {
