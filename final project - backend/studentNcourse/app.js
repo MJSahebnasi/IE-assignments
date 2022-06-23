eab31a86cefbceea35be3c0a64539a30b27e2a4c
MJSahebnasi <mjsahebnasi@gmail.com>
2022-06-23 19:31:25 +0430

    .

--- final project - backend/studentNcourse/app.js
+++ final project - backend/studentNcourse/app.js
@@ -12,6 +12,6 @@ app.use('/students', studentsRouter)
 app.use(coursesRouter)
 
 mongoose.connect('mongodb://localhost:27017/IE_final_proj')
-    .then('connected to DB.')
+    .then('Connected to DB')
     .catch(error => handleError(error));
 app.listen(3000, () => console.log('listening on port 3000 ...'))
